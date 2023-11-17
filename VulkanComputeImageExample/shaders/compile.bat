d0088d7d99afe0eab83ee0b9b7c045ee06568b5c
xiaojunwa <wxj995@gmail.com>
2023-11-17 12:40:00 -0800

    add compute image2d example

---
+++ VulkanComputeImageExample/shaders/compile.bat
@@ -0,0 +1,4 @@
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe shader_compute.vert -o vert.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe shader_compute.frag -o frag.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe shader_compute.comp -o comp.spv
+pause
