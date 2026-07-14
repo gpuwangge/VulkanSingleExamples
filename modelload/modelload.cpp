7ace3d427f94d000523ddb1c7d7775d37910a90f
Connor <wxj995@gmail.com>
2026-07-14 02:41:02 -0700

    change cmakelists to copy resource to bin/ for convenience

--- modelload/modelload.cpp
+++ modelload/modelload.cpp
@@ -32,8 +32,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "../../models/viking_room.obj";
-const std::string TEXTURE_PATH = "../../textures/viking_room.png";
+const std::string MODEL_PATH = "models/viking_room.obj";
+const std::string TEXTURE_PATH = "textures/viking_room.png";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
@@ -711,8 +711,8 @@ private:
 
     void createGraphicsPipeline()
     {
-        auto vertShaderCode = readFile("../../shaders/vert.spv");
-        auto fragShaderCode = readFile("../../shaders/frag.spv");
+        auto vertShaderCode = readFile("shaders/vert.spv");
+        auto fragShaderCode = readFile("shaders/frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
